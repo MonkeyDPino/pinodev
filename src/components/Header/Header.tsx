7f195a5185b4aee5176bcea9c928572e18f52dee
Juan Pino <juanrespolo@gmail.com>
2026-03-23 19:11:43 -0500

    feat: add language switcher to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,24 @@
+import { useTranslation } from "react-i18next";
 import HeaderSection from "../HeaderSection/HeaderSection";
+import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
 import Profile from "../Profile/Profile";
 import "./Header.scss";
 import { useState } from "react";
 
 export default function Header() {
+  const { t } = useTranslation();
   const isOpen = useState(false);
+
   return (
     <header className="header">
       <div className="header__content">
         <Profile />
         <div className="header__nav">
-          <HeaderSection title="Home" hash="home" />
-          <HeaderSection title="Experience" hash="experience" />
-          <HeaderSection title="Projects" hash="projects" />
-          <HeaderSection title="About me" hash="about_me" />
+          <HeaderSection title={t("nav_home")} hash="home" />
+          <HeaderSection title={t("nav_experience")} hash="experience" />
+          <HeaderSection title={t("nav_projects")} hash="projects" />
+          <HeaderSection title={t("nav_about")} hash="about_me" />
+          <LanguageSwitcher />
         </div>
         <div
           className="header__breadcrumb"
@@ -27,10 +32,11 @@ export default function Header() {
         </div>
         {isOpen[0] && (
           <div className="header__nav_responsive">
-            <HeaderSection title="Home" hash="home" />
-            <HeaderSection title="Experience" hash="experience" />
-            <HeaderSection title="Projects" hash="projects" />
-            <HeaderSection title="About me" hash="about_me" />
+            <HeaderSection title={t("nav_home")} hash="home" />
+            <HeaderSection title={t("nav_experience")} hash="experience" />
+            <HeaderSection title={t("nav_projects")} hash="projects" />
+            <HeaderSection title={t("nav_about")} hash="about_me" />
+            <LanguageSwitcher />
           </div>
         )}
       </div>
